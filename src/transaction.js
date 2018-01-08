9eca49bf234bba4cfb4d34110dce7389a70ea44f
technolog <viktor@allz.su>
2018-01-08 21:41:17 +0300

    transactions

--- src/transaction.js
+++ src/transaction.js
@@ -4,123 +4,123 @@ var network = require('./network');
 var leveldb = require('./leveldb');
 
 function get(req, res, next) {
-  network.getFromNode('/api/transactions/get?id=${req.params.id}', function (err, response, body) {
-    if(err) next();
-    else {
-      body = JSON.parse(body);
-      res.send(body);
-      next();
-    }
-  });
+    network.getFromNode(`/api/transactions/get?id=${req.params.id}`, function (err, response, body) {
+        if(err) next();
+        else {
+            body = JSON.parse(body);
+            res.send(body);
+            next();
+        }
+    });
 }
 
 function createBip38(req, res, next) {
-  account.getBip38Keys(req.params.userid, req.params.bip38).
+    account.getBip38Keys(req.params.userid, req.params.bip38).
     then(function(acc){
-      var transaction = sthjs.transaction.createTransaction(req.params.recipientId, req.params.amount, null, "dummy");
-      transaction.senderPublicKey = acc.keys.getPublicKeyBuffer().toString("hex");
-      delete transaction.signature;
-      sthjs.crypto.sign(transaction, acc.keys);
-      transaction.id = sthjs.crypto.getId(transaction);
-      leveldb.
+        var transaction = sthjs.transaction.createTransaction(req.params.recipientId, req.params.amount, null, "dummy");
+        transaction.senderPublicKey = acc.keys.getPublicKeyBuffer().toString("hex");
+        delete transaction.signature;
+        sthjs.crypto.sign(transaction, acc.keys);
+        transaction.id = sthjs.crypto.getId(transaction);
+        leveldb.
         setObject(transaction.id, transaction).
         then(function(){
-          res.send({
-            success: true,
-            transaction
-          });
-          next();
+            res.send({
+                success: true,
+                transaction
+            });
+            next();
         }).
         catch(function(err){
-          res.send({
-            success: false,
-            err
-          });
-          next();
+            res.send({
+                success: false,
+                err
+            });
+            next();
         });
     }).
     catch(function(err){
-      res.send({
-        success: false,
-        err
-      });
-      next();
+        res.send({
+            success: false,
+            err
+        });
+        next();
     });
 }
 
 function create(req, res, next) {
-  var amount = parseInt(req.params.amount);
-  var transaction = sthjs.transaction.createTransaction(req.params.recipientId, amount, null, req.params.passphrase);
-  leveldb.
+    var amount = parseInt(req.params.amount);
+    var transaction = sthjs.transaction.createTransaction(req.params.recipientId, amount, null, req.params.passphrase);
+    leveldb.
     setObject(transaction.id, transaction).
     then(function(){
-      res.send({
-        success: true,
-        transaction
-      });
-      next();
+        res.send({
+            success: true,
+            transaction
+        });
+        next();
     }).
     catch(function(err){
-      res.send({
-        success: false,
-        err
-      });
-      next();
+        res.send({
+            success: false,
+            err
+        });
+        next();
     });
 }
 
 function getAll(req, res, next) {
-  // Avar tx = db.get('transactions');
-  next();
+    // Avar tx = db.get('transactions');
+    next();
 }
 
 function broadcast(req, res, next) {
-  if(req.params.transactions){ //old way
-    Promise.all(
-      req.params.transactions.map((transaction) =>
-        network.broadcast(transaction, function () {
-          return Promise.resolve(transaction);
-        })
-      )
+    if(req.params.transactions){ //old way
+        Promise.all(
+            req.params.transactions.map((transaction) =>
+            network.broadcast(transaction, function () {
+                return Promise.resolve(transaction);
+            })
+        )
     ).then((transactions) => {
-      res.send({
-        success: true,
-        transactionIds: req.params.transactions.map((tx) => tx.id)
-      });
-      next();
+            res.send({
+            success: true,
+            transactionIds: req.params.transactions.map((tx) => tx.id)
     });
-  } else leveldb.getObject(req.params.id).
+        next();
+    });
+    } else leveldb.getObject(req.params.id).
     then(function(transaction){
-      transaction = transaction || req.params;
-      if (!sthjs.crypto.verify(transaction)) {
-        res.send({
-          success: false,
-          error: "transaction does not verify",
-          transaction
+        transaction = transaction || req.params;
+        if (!sthjs.crypto.verify(transaction)) {
+            res.send({
+                success: false,
+                error: "transaction does not verify",
+                transaction
+            });
+            next();
+        }
+        network.broadcast(transaction, function () {
+            res.send({
+                success: true,
+                transaction
+            });
+            next();
         });
-        next();
-      }
-      network.broadcast(transaction, function () {
-        res.send({
-          success: true,
-          transaction
-        });
-        next();
-      });
     }).
     catch(function(err){
-      res.send({
-        success: false,
-        err
-      });
-      next();
+        res.send({
+            success: false,
+            err
+        });
+        next();
     });
 }
 
 module.exports = {
-  create,
-  createBip38,
-  get,
-  broadcast,
-  getAll
+    create,
+    createBip38,
+    get,
+    broadcast,
+    getAll
 };
