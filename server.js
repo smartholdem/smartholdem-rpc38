4d9feddd70a18d7a33facef69724710d07786d7c
technolog <viktor@allz.su>
2018-01-08 21:14:19 +0300

    server

--- server.js
+++ server.js
@@ -8,14 +8,18 @@ var program = require('commander');
 
 var server = null;
 
-function restrictToLocalhost(req, res, next) {
-    if (req.getRoute().path == '/:network/broadcast' || req.connection.remoteAddress == "::1" || req.connection.remoteAddress == "127.0.0.1" || req.connection.remoteAddress == "::ffff:127.0.0.1")
+function restrictToLocalhost(req, res, next){
+    if(req.getRoute().path == '/:network/broadcast' || req.connection.remoteAddress == "::1" || req.connection.remoteAddress == "127.0.0.1" || req.connection.remoteAddress == "::ffff:127.0.0.1")
         next();
     else res.end();
 }
 
-function startServer(port) {
-    server = restify.createServer().use(restrictToLocalhost).use(restify.plugins.bodyParser({mapParams: true})).use(restify.plugins.queryParser({mapParams: true})).use(network.connect);
+function startServer(port){
+    server = restify.createServer().
+    use(restrictToLocalhost).
+    use(restify.plugins.bodyParser({mapParams: true})).
+    use(restify.plugins.queryParser({mapParams: true})).
+    use(network.connect);
 
     server.get('/:network/account/bip38/:userid', account.getBip38Account);
     server.get('/:network/account/:address', account.get);
@@ -26,14 +30,16 @@ function startServer(port) {
     server.post('/:network/transaction', transaction.create);
     server.post('/:network/broadcast', transaction.broadcast);
 
-    server.listen(port, function () {
+    server.listen(port, function() {
         console.log('smartholdem-rpc listening at %s', server.url);
     });
 }
 
-program.option('-p, --port <port>', 'The port to start server').parse(process.argv);
+program.
+option('-p, --port <port>', 'The port to start server').
+parse(process.argv);
 
-if (program.port)
+if(program.port)
     startServer(program.port);
 else
     startServer(8081);
