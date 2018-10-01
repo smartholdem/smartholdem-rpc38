4b1d104e835e05f3b1d258b3294af1f74a030616
technolog <viktor@allz.su>
2018-10-01 19:00:26 +0300

    mainnet seeds update

--- src/network.js
+++ src/network.js
@@ -1,11 +1,11 @@
-var request = require('request');
-var async = require('async');
-var sthjs = require('sthjs');
+const request = require('request');
+const async = require('async');
+const sthjs = require('sthjs');
 
 var network = null,
-  server = null;
+    server = null;
 
-var networks = {
+const networks = {
     devnet: {
         name: "devnet",
         nethash: "3a6d2bec6798dedea99a1e6c64120a3876781b85e46bb75908aba07ffda61360",
@@ -33,136 +33,136 @@ var networks = {
 };
 
 function getFromNode(url, cb) {
-  var nethash = network ? network.nethash : "";
-  if (!url.startsWith("http")) {
-    url = `http://${server}${url}`;
-  }
-  request(
-    {
-      url,
-      headers: {
-        nethash,
-        version: '1.0.0',
-        port: 1
-      },
-      timeout: 5000
-    },
-    function(error, response, body){
-      if(error){
-        server = network.peers[Math.floor(Math.random() * 1000) % network.peers.length];
-      }
-      cb(error, response, body);
+    console.log(getFromNode);
+
+    let nethash = network ? network.nethash : "";
+    if (!url.startsWith("http")) {
+        url = `http://${server}${url}`;
     }
-  );
+    request(
+        {
+            url,
+            headers: {
+                nethash,
+                version: '1.0.0',
+                port: 1
+            },
+            timeout: 5000
+        },
+        function (error, response, body) {
+            if (error) {
+                server = network.peers[Math.floor(Math.random() * 1000) % network.peers.length];
+            }
+            cb(error, response, body);
+        }
+    );
 }
 
 function findEnabledPeers(cb) {
-  var peers = [];
-  getFromNode('/peer/list', function (err, response, body) {
-    if (err || body === "undefined") {
-      cb(peers);
-    }
-    var respeers = JSON.parse(body).peers.
-    filter(function (peer) {
-      return peer.status === "OK";
-    }).
-    map(function (peer) {
-      return `${peer.ip}:${peer.port}`;
-    });
-    async.each(respeers, function (peer, eachcb) {
-      getFromNode(`http://${peer}/api/blocks/getHeight`, function (error, res, body2) {
-        if (!error && body2 != "Forbidden") {
-          peers.push(peer);
+    var peers = [];
+    getFromNode('/peer/list', function (err, response, body) {
+        if (err || body === "undefined") {
+            cb(peers);
         }
-        eachcb();
-      });
-    }, function (error) {
-      if (error) return cb(error);
+        var respeers = JSON.parse(body).peers.filter(function (peer) {
+            return peer.status === "OK";
+        }).map(function (peer) {
+            return `${peer.ip}:${peer.port}`;
+        });
+        async.each(respeers, function (peer, eachcb) {
+            getFromNode(`http://${peer}/api/blocks/getHeight`, function (error, res, body2) {
+                if (!error && body2 != "Forbidden") {
+                    peers.push(peer);
+                }
+                eachcb();
+            });
+        }, function (error) {
+            if (error) return cb(error);
 
-      return cb(peers);
+            return cb(peers);
+        });
+
     });
-
-  });
 }
 
 function postTransaction(transaction, cb) {
-  request(
-    {
-      url: `http://${server}/peer/transactions`,
-      headers: {
-        nethash: network.nethash,
-        version: '1.0.0',
-        port: 1
-      },
-      method: 'POST',
-      json: true,
-      body: {transactions: [transaction]}
-    },
-    cb
-  );
+    request(
+        {
+            url: `http://${server}/peer/transactions`,
+            headers: {
+                nethash: network.nethash,
+                version: '1.0.0',
+                port: 1
+            },
+            method: 'POST',
+            json: true,
+            body: {transactions: [transaction]}
+        },
+        cb
+    );
 }
 
 function broadcast(transaction, callback) {
-  network.peers.slice(0, 10).forEach(function (peer) {
-    // Console.log("sending to", peer);
-    request({
-      url: `http://${peer}/peer/transactions`,
-      headers: {
-        nethash: network.nethash,
-        version: '1.0.0',
-        port: 1
-      },
-      method: 'POST',
-      json: true,
-      body: {transactions: [transaction]}
+    network.peers.slice(0, 10).forEach(function (peer) {
+        // Console.log("sending to", peer);
+        request({
+            url: `http://${peer}/peer/transactions`,
+            headers: {
+                nethash: network.nethash,
+                version: '1.0.0',
+                port: 1
+            },
+            method: 'POST',
+            json: true,
+            body: {transactions: [transaction]}
+        });
     });
-  });
-  callback();
+    callback();
 }
 
 
 function connect2network(netw, callback) {
-  network = netw;
-  server = netw.peers[Math.floor(Math.random() * 1000) % netw.peers.length];
-  findEnabledPeers(function (peers) {
-    if (peers.length > 0) {
-      // console.log(peers);
-      [server] = peers;
-      netw.peers = peers;
-    }
-    callback();
-  });
-  getFromNode('/api/loader/autoconfigure', function (err, response, body) {
-    if (err) return;
-    if (!body || !body.startsWith("{"))
-      connect2network(netw, callback);
-    else {
-      netw.config = JSON.parse(body).network;
-    }
-  });
+    network = netw;
+    server = netw.peers[Math.floor(Math.random() * 1000) % netw.peers.length];
+    findEnabledPeers(function (peers) {
+        if (peers.length > 0) {
+            // console.log(peers);
+            [server] = peers;
+            netw.peers = peers;
+        }
+        callback();
+    });
+    getFromNode('/api/loader/autoconfigure', function (err, response, body) {
+        if (err) return;
+        if (!body || !body.startsWith("{"))
+            connect2network(netw, callback);
+        else {
+            netw.config = JSON.parse(body).network;
+        }
+    });
 }
 
 function connect(req, res, next) {
-  if (!server || !network || network.name != req.params.network) {
-    if (networks[req.params.network]) {
-      sthjs.crypto.setNetworkVersion(networks[req.params.network].version);
-      connect2network(networks[req.params.network], next);
+    if (!server || !network || network.name != req.params.network) {
+        if (networks[req.params.network]) {
+            sthjs.crypto.setNetworkVersion(networks[req.params.network].version);
+            connect2network(networks[req.params.network], next);
+        } else {
+            res.send({
+                success: false,
+                error: `Could not find network ${req.params.network}`
+            });
+            res.end();
+        }
     } else {
-      res.send({
-        success: false,
-        error: `Could not find network ${req.params.network}`
-      });
-      res.end();
+        next();
     }
-  } else {
-    next();
-  }
 }
 
 
 module.exports = {
-  broadcast,
-  connect,
-  getFromNode,
-  postTransaction
+    broadcast,
+    connect,
+    getFromNode,
+    postTransaction
 };
